ca15daddd761d0fb6161725db5e334fb97982450
bellom <bellomsean@gmail.com>
2019-04-15 10:47:06 +0100

    player class added

---
+++ lib/player.rb
@@ -0,0 +1,13 @@
+class Player
+    attr_reader   :name, :token, :cells_chosen
+  
+    def initialize(name, token)
+      @name = name
+      @token = token
+      @cells_chosen = []
+    end
+  
+    def add_cell(cell_number)
+      @cells_chosen.push(cell_number)
+    end
+end
